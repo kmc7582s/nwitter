c3af6aa45804bfffe06612532d0f8c2cbb78abd3
MinChan <76563710+kmc7582s@users.noreply.github.com>
2022-02-22 23:53:16 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 function App() {
   return (
     <div>App</div>
---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJU13X9aeb5xJT3blaJbbnYQ1XjBqQrso",
+    authDomain: "nwitter-6847a.firebaseapp.com",
+    projectId: "nwitter-6847a",
+    storageBucket: "nwitter-6847a.appspot.com",
+    messagingSenderId: "71843078670",
+    appId: "1:71843078670:web:87355cb7ca803f6f86d25f"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
+
+
+//new
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDJU13X9aeb5xJT3blaJbbnYQ1XjBqQrso",
+//   authDomain: "nwitter-6847a.firebaseapp.com",
+//   projectId: "nwitter-6847a",
+//   storageBucket: "nwitter-6847a.appspot.com",
+//   messagingSenderId: "71843078670",
+//   appId: "1:71843078670:web:87355cb7ca803f6f86d25f"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
